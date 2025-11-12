0f631f3dbfe9afe2c943d3b53c5c9b2dd5718810
Soham Kundu <153625511+TheSK653@users.noreply.github.com>
2025-11-12 14:50:32 +0530

    Add files via upload

---
+++ swiggi_01 - Copy/src/components/RestaurantCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function RestaurantCard({ restaurant }) {
+    return (
+        <Link to={`/restaurant  /${restaurant.info.name}`} >
+            <div className='m-2 w-58'>
+                <img className='w-58 h-42 rounded-2xl' src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${restaurant.info.cloudinaryImageId}`} />
+                <div className='ml-4'>
+                    <h1 className='font-bold text-xl'>{restaurant.info.name}</h1>
+                    <span className='font-bold'>{restaurant.info.avgRating}</span>
+                    <span className='font-bold mx-4'>{restaurant.info.sla.slaString}</span>
+                    <p>{restaurant.info.cuisines[0]}</p>
+                    <p>{restaurant.info.locality}</p>
+                </div>
+            </div>
+        </Link>
+    )
+}
+
+export default RestaurantCard
---
+++ swiggi_01 - Copy/src/components/RestaurantDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import {}
+
+function RestaurantDetails() {
+    return (
+        <div>Restaurant Details</div>
+    )
+}
+
+export default RestaurantDetails
